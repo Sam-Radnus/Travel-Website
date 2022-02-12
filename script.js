e79fcd8aba5cdef82f18f3552822a2997d879b9b
samsundar <samradnus2001@gmail.com>
2022-02-12 22:51:18 +0530

    partial commit

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 const cursor=document.querySelector('.cursor');
 document.addEventListener('mousemove',e=>
 {
-    // console.log(e);
     cursor.setAttribute("style","top:"+(e.pageY-35)+"px; left:"+(e.pageX-35)+"px;")
 })
 document.addEventListener('click',()=>{
@@ -10,4 +9,22 @@ document.addEventListener('click',()=>{
     setTimeout(()=>{
         cursor.classList.remove("expand");
     },500);
+})
+const buttons=document.querySelectorAll('button.read');
+console.log(buttons);
+buttons.forEach(btn=>{
+    btn.addEventListener('click',function(e)
+    {
+
+        let x=e.clientX-e.target.offsetLeft;
+        let y=e.clientY-e.target.offsetTop;
+        let ripples=document.createElement('span');
+        ripples.classList='ripple';
+        ripples.style.left=x+'px';
+        ripples.style.top=y+'px';
+        this.appendChild(ripples);
+         setTimeout(()=>{
+             ripples.remove();
+        },1000)
+    })
 })
